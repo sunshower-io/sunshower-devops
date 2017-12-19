009a33e908d82cf5048ff05071d77cee0488cd2a
josiahhaswell <josiah.haswell@gmail.com>
2017-12-19 15:59:09 -0700

    Fixing path issue

--- scripts/publish-env.sh
+++ scripts/publish-env.sh
@@ -6,7 +6,7 @@ parse_version $1
 
 mvn clean install -f sunshower-env/pom.xml
 mvn versions:set -DnewVersion=${VERSION} -f sunshower-env/pom.xml
-mvn clean install -f sunsower-env/parent/pom.xml
+mvn clean install -f sunshower-env/parent/pom.xml
 mvn versions:set -DnewVersion=${VERSION} -f sunshower-env/parent/pom.xml
 mvn clean install deploy -f sunshower-env/pom.xml
 mvn clean install deploy -f sunshower-env/parent/pom.xml
