905006634bc4d70653277317a7571f4d6acc1a39
Josiah Haswell <josiah.haswell@gmail.com>
2018-05-07 16:02:52 -0600

    fixing deployment order

--- scripts/publish-env.sh
+++ scripts/publish-env.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -eu
 
 
-source ./scripts/set-version.sh
+#source ./scripts/set-version.sh
 
-parse_version $1
+#parse_version $1
 #export GPG_TTY=$(tty)
 
 #echo "$GPG_ASC" | base64 -d | gpg --batch --passphrase=$GPG_PASSPHRASE --allow-secret-key-import --import
 
 #POM_VERSION=echo '${project.version}\n0\n' | mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate | grep '^VERSION'
 #echo "Using Project Version:${POM_VERSION}"
-mvn clean install -f sunshower-env/parent/pom.xml
-mvn clean install deploy  -f sunshower-env/pom.xml
-mvn clean install deploy  -f sunshower-env/parent/pom.xml
+mvn clean install deploy -f sunshower-env/pom.xml
+mvn clean install deploy -f sunshower-env/parent/pom.xml
+mvn clean install deploy -f sunshower-env/parent/pom.xml
