a99b3986dffa947255890be78aedd6462d06bda6
Josiah Haswell <josiah.haswell@gmail.com>
2018-09-21 13:37:30 -0600

    Testing release

--- scripts/publish-env.sh
+++ scripts/publish-env.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -eu
 
 
-#source ./scripts/set-version.sh
+source ./scripts/set-version.sh
 
-#parse_version $1
+parse_version $1
 #export GPG_TTY=$(tty)
 
 #echo "$GPG_ASC" | base64 -d | gpg --batch --passphrase=$GPG_PASSPHRASE --allow-secret-key-import --import
 
 #POM_VERSION=echo '${project.version}\n0\n' | mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate | grep '^VERSION'
-#echo "Using Project Version:${POM_VERSION}"
 echo "Using Maven Profile: ${MAVEN_PROFILE}"
 mvn clean install deploy -f sunshower-env/pom.xml -P ${MAVEN_PROFILE}
 mvn clean install deploy -f sunshower-env/parent/pom.xml -P ${MAVEN_PROFILE}
