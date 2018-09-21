7f6ef5b04a43cdbddcfa075a32061293358cd4cc
Josiah Haswell <josiah.haswell@gmail.com>
2018-09-21 16:16:18 -0600

    test

--- scripts/publish-env.sh
+++ scripts/publish-env.sh
@@ -10,14 +10,15 @@ source ./scripts/set-version.sh
 POM_VERSION=$(echo 'VERSION=${project.version}' | mvn -f sunshower-env/pom.xml org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate | grep '^VERSION' | cut -f2 -d= | cut -f1 -d "-")
 echo "POM Version: ${POM_VERSION}"
 
-VERSION=$(parse_version $POM_VERSION)
+C_VERSION=$(parse_version $POM_VERSION)
+VERSION=$C_VERSION
 echo "Using Maven Profile: ${MAVEN_PROFILE}"
 echo "Using version: ${VERSION}"
 mvn clean install deploy -f sunshower-env/pom.xml -P ${MAVEN_PROFILE}
 mvn clean install deploy -f sunshower-env/parent/pom.xml -P ${MAVEN_PROFILE}
 
 if [ "$IS_RELEASE" = "true" ]; then
-    NEXT_VERSION=$(increment_version $NEXT_VERSION);
+    NEXT_VERSION=$(increment_version $C_VERSION);
     echo "Setting version: $VERSION -> $NEXT_VERSION"
     mvn versions:set -f sunshower-env/pom.xml -DnewVersion=$VERSION;
     mvn versions:set -f sunshower-env/parent/pom.xml -DnewVersion=$VERSION;
