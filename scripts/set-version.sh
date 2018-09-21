decc0f285e7d8ce01f22497f3aa1e7f4634f4f1f
Josiah Haswell <josiah.haswell@gmail.com>
2018-09-21 15:41:50 -0600

    testing again

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -9,7 +9,7 @@ IS_RELEASE="true"
 NEXT_VERSION=""
 
 increment_version() {
-   NEXT_VERSION=echo "$1" | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'
+   echo "$1" | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'
 }
 
 parse_version() {
