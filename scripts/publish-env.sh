f499ee24d41bcf09acaf90eba040f0257c044662
Josiah Haswell <josiah.haswell@gmail.com>
2018-09-21 17:58:43 -0600

    testing release

--- scripts/publish-env.sh
+++ scripts/publish-env.sh
@@ -23,19 +23,3 @@ echo "Next version: $NEXT_VERSION";
 mvn clean install deploy -f sunshower-env/pom.xml -P ${MAVEN_PROFILE}
 mvn clean install deploy -f sunshower-env/parent/pom.xml -P ${MAVEN_PROFILE}
 
-if [ "$BRANCH_NAME" = "master" ]; then
-    mvn versions:set -f sunshower-env/pom.xml -DnewVersion=$VERSION;
-    mvn versions:set -f sunshower-env/parent/pom.xml -DnewVersion=$VERSION;
-    mvn clean install deploy -f sunshower-env/pom.xml -P ${MAVEN_PROFILE};
-    mvn clean install deploy -f sunshower-env/parent/pom.xml -P ${MAVEN_PROFILE};
-    mvn versions:set -f sunshower-env/pom.xml -DnewVersion=$NEXT_VERSION -P ${MAVEN_PROFILE};
-    mvn versions:set -f sunshower-env/parent/pom.xml -DnewVersion="${NEXT_VERSION}-SNAPSHOT" -P ${MAVEN_PROFILE};
-    git remote set-url origin https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/sunshower-io/sunshower-devops
-    git config user.email "${GITHUB_USERNAME}@sunshower.io"
-    git config user.name "${GITHUB_USERNAME}"
-    git config user.password "${GITHUB_PASSWORD}"
-    git checkout -b master
-    git commit -am "Releasing new version ${VERSION}"
-    git tag -a v$VERSION -m "Released version ${VERSION}"
-    git push origin --tags
-fi;
