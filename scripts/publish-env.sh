242bda53b31d2e3dd47de37caf703adfc0ec3465
Josiah Haswell <josiah.haswell@gmail.com>
2018-09-21 15:59:20 -0600

    testing

--- scripts/publish-env.sh
+++ scripts/publish-env.sh
@@ -10,7 +10,7 @@ source ./scripts/set-version.sh
 POM_VERSION=$(echo 'VERSION=${project.version}\n0\n' | mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate | grep '^VERSION' | cut -f2 -d=)
 echo "POM Version: ${POM_VERSION}"
 
-VERSION=$(parse_version) $POM_VERSION
+VERSION=$(parse_version $POM_VERSION)
 echo "Using Maven Profile: ${MAVEN_PROFILE}"
 echo "Using version: ${VERSION}"
 mvn clean install deploy -f sunshower-env/pom.xml -P ${MAVEN_PROFILE}
