10f32ac1f13a2a3818b73d8f504752c9b6753eac
Josiah Haswell <josiah.haswell@gmail.com>
2018-09-21 16:20:57 -0600

    again

--- scripts/publish-env.sh
+++ scripts/publish-env.sh
@@ -12,14 +12,17 @@ echo "POM Version: ${POM_VERSION}"
 
 C_VERSION=$(parse_version $POM_VERSION)
 VERSION=$C_VERSION
+NEXT_VERSION=$(increment_version $C_VERSION)
+
+
 echo "Using Maven Profile: ${MAVEN_PROFILE}"
 echo "Using version: ${VERSION}"
+echo "Next version: $NEXT_VERSION";
+
 mvn clean install deploy -f sunshower-env/pom.xml -P ${MAVEN_PROFILE}
 mvn clean install deploy -f sunshower-env/parent/pom.xml -P ${MAVEN_PROFILE}
 
 if [ "$IS_RELEASE" = "true" ]; then
-    NEXT_VERSION=$(increment_version $C_VERSION);
-    echo "Setting version: $VERSION -> $NEXT_VERSION"
     mvn versions:set -f sunshower-env/pom.xml -DnewVersion=$VERSION;
     mvn versions:set -f sunshower-env/parent/pom.xml -DnewVersion=$VERSION;
     mvn clean install deploy -f sunshower-env/pom.xml -P ${MAVEN_PROFILE};
