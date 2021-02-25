4c49b74857612f34c79f7d95eabdf7cfbe0fbf69
Josiah Haswell <josiah.haswell@gmail.com>
2021-02-25 16:51:28 -0700

    Adding support for username/password

--- infrastructure/proxmox/k8s/base/scripts/docker-daemon.sh
+++ infrastructure/proxmox/k8s/base/scripts/docker-daemon.sh
@@ -5,3 +5,14 @@ cat <<-EOF > /etc/docker/daemon.json
 }
 EOF
 sudo systemctl restart docker
+
+
+until [ "$(systemctl is-active docker)" = 'active' ]; do
+
+  echo "waiting for docker daemon to start..."
+  sleep 1
+done
+
+echo "Docker started.  Logging in"
+# todo: add docker login --username <username> password <password>
+echo "Logged in"
